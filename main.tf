d01c782e350a1bbd7558605a8f330ab8c0652525
dpathim <dpdevops6@gmail.com>
2023-09-12 16:23:20 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -9,11 +9,15 @@
 #  value = var.test
 #}
 
-module "components" {
+module "vpc" {
 
   source     = "git::https://github.com/pdeepthi562/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
-  subnets =each.value["subnets"]
+  subnets = each.value["subnets"]
+}
+
+output "vpc" {
+  value = module.vpc
 }
