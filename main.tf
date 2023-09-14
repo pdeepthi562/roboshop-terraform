1391529f3be7df7b4631d777ed74417e5807d86f
dpathim <dpdevops6@gmail.com>
2023-09-13 20:27:14 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "vpc" {
   for_each   = var.vpc
   cidr       = each.value["cidr"]
   subnets    = each.value["subnets"]
-  default_vpc_id = var.default_vpc_id
+  #default_vpc_id = var.default_vpc_id
 }
 
 output "vpc" {
