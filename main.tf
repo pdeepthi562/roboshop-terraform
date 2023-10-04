12aec60e210bb90cb21e7933a42ee60bb85210f7
dpathim <dpdevops6@gmail.com>
2023-10-04 18:20:42 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -117,6 +117,7 @@ module "rabbitmq" {
 
 
 module "app" {
+  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
   source = "git::https://github.com/pdeepthi562/tf-module-app.git"
   tags              = var.tags
   env               = var.env
